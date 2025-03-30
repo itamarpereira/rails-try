34892988e374d5750e72745b3f19085473d3bc0b
Itamar Pereira <itamartpereira@hotmail.com>
2025-03-30 10:12:31 -0300

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning the database"
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts "DB is clean"
+
+puts "Creating some ingredients"
+
+Ingredient.create(name: "Mint leaves")
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Cachaça")
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "Orange Juice")
+Ingredient.create(name: "Tomato Juice")
+Ingredient.create(name: "Coke")
+
+puts "Done!"
